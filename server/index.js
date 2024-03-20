cc923282f190c630054eb2a2e82412517f2ec87d
Avnish Uniyal <avnish5128uniyal@gmail.com>
2024-03-20 22:34:52 +0530

    cors changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://we-doo-conferencing-client.vercel.app'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
