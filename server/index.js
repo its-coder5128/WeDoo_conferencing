a1bedcfd9933aef29bf92f39b7b873ef3a6aedcf
Avnish Uniyal <avnish5128uniyal@gmail.com>
2024-03-20 21:04:56 +0530

    server commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const server = app.listen(port,()=>{
 
 const io = new Server(server, {
   cors: {
-    origin:[ "http://localhost:3000"],
+    origin:[ "https://we-doo-conferencing-client.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
